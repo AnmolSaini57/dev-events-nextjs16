53b15b4e008f879ade5c1ecd3f6ab6388c8026cf
AnmolSaini57 <anmolsaini5890@gmail.com>
2026-01-24 23:50:49 +0530

    specific event api, gettin data at the frontend using dynamic routing

---
+++ app/api/events/[slug]/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import Event, { IEvent } from "@/database/event.model";
+import connectDB from "@/lib/mongodb";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ slug: string }> },
+): Promise<NextResponse> {
+  try {
+    await connectDB();
+
+    const { slug } = await params;
+    console.log(req);
+
+    if (!slug || typeof slug !== "string" || slug.trim() === "") {
+      return NextResponse.json(
+        { message: "Invalid or missing slug parameter" },
+        { status: 400 },
+      );
+    }
+
+    const sanitizedSlug = slug.trim().toLowerCase();
+
+    const event: IEvent = await Event.findOne({ slug: sanitizedSlug }).lean();
+
+    if (!event) {
+      return NextResponse.json({ message: "Event not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      {
+        message: "Event fetched successfully",
+        event,
+      },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      {
+        message: "Failed to fetch event",
+        error: e instanceof Error ? e.message : "Unknown Error",
+      },
+      { status: 500 },
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import { events, EventType } from "@/lib/constants";
+import { IEvent } from "@/database/event.model";
 
-function Home() {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Home = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
-        The Hub For Every Dev <br /> Event You Can't Miss!
+        The Hub For Every Dev <br /> Event You Can&apos;t Miss!
       </h1>
       <p className="text-center mt-5">
         Hackathons, Conferences, and Meetups, All in One Place.
@@ -18,15 +23,17 @@ function Home() {
         <h3>Feature Events</h3>
 
         <ul className="events list-none">
-          {events.map((event: EventType) => (
-            <li key={event.title}>
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title}>
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
   );
-}
+};
 
 export default Home;
