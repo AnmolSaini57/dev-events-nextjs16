74c31ad84b98b2875bd83ce4104f19c1ee59beb1
AnmolSaini57 <anmolsaini5890@gmail.com>
2026-01-30 01:06:01 +0530

    fix static caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database/event.model";
